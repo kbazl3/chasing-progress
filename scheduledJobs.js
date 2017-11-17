63f37fb426313c273f2ff7843ecfc2f7f05f0051
kbazl3 <kbarlowjs@gmail.com>
2017-11-16 17:55:19 -0700

    added percent completed function to scheduler

--- scheduledJobs.js
+++ scheduledJobs.js
@@ -1,29 +1,41 @@
 const dailyTodoCtrl = require('./controllers/dailyTodoCtrl'),
-      mongoose = require('mongoose'),
-      request = require('request');
+    mongoose = require('mongoose'),
+    request = require('request');
 
 mongoose.Promise = global.Promise;
 
-request.get("https://cryptic-ravine-99712.herokuapp.com/api/dailyList", function(error, response, body) {
-  let parsedJson = JSON.parse(body);
-  request({
-      url: "https://cryptic-ravine-99712.herokuapp.com/api/dailyLogs",
-      method: 'POST',
-      json: { tasks: parsedJson.dailyTasks }
-  }, function(error, request, body) {
-      console.log("create daily log xxxxxxxxxxxxxxxxxxxxxx", body);
-      console.log("error xxxxxxxxxxxxxxxxxxxxxx", error);
-  })
-  parsedJson.dailyTasks.forEach(function(task) {
-      task.completed = false;
-      request({
-          url: "https://cryptic-ravine-99712.herokuapp.com/api/dailyList/" + task._id,
-          method: 'PUT',
-          json: task
-      }, function(error, request, bodys) {
+const percentCompleted = function(ary) {
+    let complete = 0;
+    ary.forEach(function(task) {
+        if (task.completed) {
+            complete++;
+        }
+    })
+    return (complete / ary.length) * 100;
+}
 
-          })
-  })
+request.get("https://cryptic-ravine-99712.herokuapp.com/api/dailyList", function(error, response, body) {
+    let parsedJson = JSON.parse(body);
+    request({
+        url: "https://cryptic-ravine-99712.herokuapp.com/api/dailyLogs",
+        method: 'POST',
+        json: {
+            tasks: parsedJson.dailyTasks,
+            percentCompleted: percentCompleted(parsedJson.dailyTasks)
+        }
+    }, function(error, request, body) {
+        
+    })
+    parsedJson.dailyTasks.forEach(function(task) {
+        task.completed = false;
+        request({
+            url: "https://cryptic-ravine-99712.herokuapp.com/api/dailyList/" + task._id,
+            method: 'PUT',
+            json: task
+        }, function(error, request, bodys) {
+
+        })
+    })
 });
 
 // dailyTodoCtrl.getDailyTaskszzz()
