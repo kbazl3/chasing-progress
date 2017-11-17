999369798a9c1a77948d1c2940cc6579ab4a45eb
kbazl3 <kbarlowjs@gmail.com>
2017-11-16 17:19:44 -0700

    adding daily log post request from scheduled jobs

--- scheduledJobs.js
+++ scheduledJobs.js
@@ -5,29 +5,28 @@ const dailyTodoCtrl = require('./controllers/dailyTodoCtrl'),
 mongoose.Promise = global.Promise;
 
 request.get("https://cryptic-ravine-99712.herokuapp.com/api/dailyList", function(error, response, body) {
-    console.log(body);
-  let parsedJson = JSON.parse(body);
-  // request({
-  //     url: "https://cryptic-ravine-99712.herokuapp.com/api/dailyLogs",
-  //     method: 'PUT',
-  //     json: parsedJson.dailyTasks
-  // }, function(error, request, body) {
-  //     console.log("create daily log", body);
-  // })
-  parsedJson.dailyTasks.forEach(function(task) {
+  let json = JSON.parse(body);
+  json.dailyTasks.forEach(function(task) {
       task.completed = false;
       request({
           url: "https://cryptic-ravine-99712.herokuapp.com/api/dailyList/" + task._id,
           method: 'PUT',
           json: task
       }, function(error, request, bodys) {
+              console.log(bodys);
           })
   })
 });
 
-console.log("also hitting this");
+console.log("also hitting thi");
 
-
+// request({
+//     url: "https://cryptic-ravine-99712.herokuapp.com/api/dailyLogs",
+//     method: 'PUT',
+//     json: parsedJson.dailyTasks
+// }, function(error, request, body) {
+//     console.log("create daily log", body);
+// })
 
 // dailyTodoCtrl.getDailyTaskszzz()
 // console.log(dailyTodoCtrl.dailyTodoObject);
